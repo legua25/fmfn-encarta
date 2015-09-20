d89d7b017a2b5fef9a5dcf4c9b8df6c31c668710
A Carrot <legua.2507@gmail.com>
2015-09-20 16:58:23 -0500

    Update: added classifier models

--- apps/fmfn/models/classifiers.py
+++ apps/fmfn/models/classifiers.py
@@ -3,4 +3,43 @@ from __future__ import unicode_literals
 from django.utils.translation import ugettext_lazy as _
 from django.db.models import *
 
+class MaterialTag(Model):
 
+	name = CharField(
+		max_length = 64,
+		verbose_name = _('type name')
+	)
+	description = CharField(
+		max_length = 256,
+		verbose_name = _('type description')
+	)
+	materials = ForeignKey('fmfn.Material',
+		related_name = 'themes',
+		on_delete = CASCADE,
+		verbose_name = _('tagged materials')
+	)
+
+	class Meta(object):
+		abstract = True
+
+class Type(MaterialTag):
+
+	class Meta(object):
+
+		verbose_name = _('material type')
+		verbose_name_plural = _('material types')
+		app_label = 'fmfn'
+class Theme(MaterialTag):
+
+	class Meta(object):
+
+		verbose_name = _('material type')
+		verbose_name_plural = _('material types')
+		app_label = 'fmfn'
+class Language(MaterialTag):
+
+	class Meta(object):
+
+		verbose_name = _('material language')
+		verbose_name_plural = _('material languages')
+		app_label = 'fmfn'
