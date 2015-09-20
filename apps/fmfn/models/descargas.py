0e4901b6436535361356a479d1d7cc28e22fdcd4
AnGlez <anaglezr13@gmail.com>
2015-09-20 18:54:24 -0500

    borre un archivo en español

--- apps/fmfn/models/descargas.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.utils.translation import ugettext_lazy as _
-from django.db.models import *
-from django.conf import settings
