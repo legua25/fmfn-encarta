33f7de74da751e8e29e7d8f069761453a5340b32
AnGlez <anaglezr13@gmail.com>
2015-09-20 19:45:29 -0500

    models documented

--- apps/fmfn/models/portfolios.py
+++ apps/fmfn/models/portfolios.py
@@ -6,19 +6,20 @@ from django.conf import settings
 from _base import Model
 
 class Portfolio(Model):
-
-    materials = ManyToManyField('fmfn.Material',
+	"""" Registry of materials marked as favorite by a user
+	"""
+	materials = ManyToManyField('fmfn.Material',
         related_name = '+',
         verbose_name = _('materials')
     )
 
-    user = ForeignKey(settings.AUTH_USER_MODEL,
+	user = ForeignKey(settings.AUTH_USER_MODEL,
         related_name = 'portfolio',
         verbose_name = _('user')
     )
 
-    class Meta(object):
+	class Meta(object):
 
-        verbose_name = _('portfolio')
-        verbose_name_plural = _('portfolios')
-        app_label = 'fmfn'
+		verbose_name = _('portfolio')
+		verbose_name_plural = _('portfolios')
+		app_label = 'fmfn'
