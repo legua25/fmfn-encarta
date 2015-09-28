ba95bbf9ff927f903cc82927ef7b5465451b536d
A Carrot <legua.2507@gmail.com>
2015-09-27 22:29:50 -0500

    Update: fixed error for test execution, adapted tests to override CSRF verification (impossible to mock otherwise)

--- apps/fmfn/tests/accounts.py
+++ apps/fmfn/tests/accounts.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.core.urlresolvers import reverse_lazy
-from django.test import TestCase, RequestFactory
 from django.contrib.auth import get_user_model
-from apps.fmfn.views import login
+from django.test import TestCase, Client
 
 __all__ = [ 'LoginTest' ]
 User = get_user_model()
@@ -13,7 +12,7 @@ class LoginTest(TestCase):
 	def setUp(self):
 
 		# Create a request factory
-		self.factory = RequestFactory()
+		self.client = Client()
 
 		# Create a test user
 		User.objects.create_user(
@@ -25,36 +24,34 @@ class LoginTest(TestCase):
 	def test_login_invalid_user(self):
 
 		# Create a mock request (type: POST, arguments from actual form)
-		request = self.factory.post(reverse_lazy('index'), data = {
+		response = self.client.post(reverse_lazy('login'), data = {
 			'email': 'a@a.com',
 			'password': 'asdfg123'
 		})
-		request.user = None
 
-		# Executes the view code, builds response
-		response = login(request)
 		# Upon failure, status code is 401 - test this
 		self.assertEqual(response.status_code, 401)
 	def test_login_valid_user(self):
 
 		# Create a mock request (type: POST, arguments from actual form)
-		request = self.factory.post(reverse_lazy('index'), data = {
+		response = self.client.post(reverse_lazy('login'), data = {
 			'email': 'email@example.com',
 			'password': 'asdfg123'
 		})
-		request.user = None
 
-		# Executes the view code, builds response
-		response = login(request)
-		# User is redirected upon successful login - verify this by status code 301
-		self.assertEqual(response.status_code, 301)
+		# User is redirected upon successful login - verify this by status code 302
+		self.assertEqual(response.status_code, 302)
 	def test_already_logged_in(self):
 
 		# Create a mock request (type: POST, arguments from actual form)
-		request = self.factory.post(reverse_lazy('index'))
-		request.user = User.objects.get(email = 'email@example.com')
+		response = self.client.post(reverse_lazy('login'), data = {
+			'email': 'email@example.com',
+			'password': 'asdfg123'
+		})
 
-		# Executes the view code, builds response
-		response = login(request)
-		# User is redirected upon successful login - verify this by status code 301
-		self.assertEqual(response.status_code, 301)
+		# User is redirected upon successful login - verify this by status code 302
+		self.assertEqual(response.status_code, 302)
+
+		# User is redirected because he/she is already logged in - verify this by status code 302
+		response = self.client.post(reverse_lazy('login'))
+		self.assertEqual(response.status_code, 302)
--- fmfn_encarta/settings/__init__.py
+++ fmfn_encarta/settings/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from develop import Development
+# from develop import Development
 # from product import Production
-# from testing import Testing
+
+# Fallback for testing issues
+from testing import *
