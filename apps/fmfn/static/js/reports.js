440d390e5172ebebed375d6a8607b1ec7a72de2c
PoloGarcia <laga.1193@gmail.com>
2015-11-04 10:23:15 -0600

    Added JS validation to report modal

--- apps/fmfn/static/js/reports.js
+++ apps/fmfn/static/js/reports.js
@@ -3,22 +3,27 @@ $(document).on('click', '#insert-report', function () {
     var csfrToken = $("input[name=csrfmiddlewaretoken]").val();
     var material = $("input[name=report-material-id]").val();
     var url = '/manage/reports/create/' + material + '/';
-    $.ajax({
-        url: url,
-        type: "POST",
-        data: {
-            csrfmiddlewaretoken: csfrToken,
-            description: description
-        },
-        success: function (response) {
-            $('#report-content').val('');
-            $('#success-report').foundation('reveal', 'open');
-            $('#report-material').foundation('reveal', 'close');
-        },
-        error: function (xhr, textStatus, thrownError) {
-            alert('Error:' + xhr.statusText);
-        }
-    });
+    if (description == 0) {
+        $('#report-error').show();
+    } else {
+        $('#report-error').hide();
+        $.ajax({
+            url: url,
+            type: "POST",
+            data: {
+                csrfmiddlewaretoken: csfrToken,
+                description: description
+            },
+            success: function (response) {
+                $('#report-content').val('');
+                $('#success-report').foundation('reveal', 'open');
+                $('#report-material').foundation('reveal', 'close');
+            },
+            error: function (xhr, textStatus, thrownError) {
+                alert('Error:' + xhr.statusText);
+            }
+        });
+    }
 });
 
 $(document).on('click', '#show-report-material', function () {
@@ -79,7 +84,7 @@ $(document).on('click', '.reject-report', function () {
     });
 });
 
-$(document).on('click', '.report-notif-close',function(){
+$(document).on('click', '.report-notif-close', function () {
     var notification = this.parentNode;
     notification.remove();
 });
