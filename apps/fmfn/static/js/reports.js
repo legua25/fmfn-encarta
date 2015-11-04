e56b20ea58c44f3eed0a7555248c7454e71f5a30
PoloGarcia <laga.1193@gmail.com>
2015-11-04 02:16:07 -0600

    Report functionality done

--- apps/fmfn/static/js/reports.js
+++ apps/fmfn/static/js/reports.js
@@ -13,7 +13,7 @@ $(document).on('click', '#insert-report', function () {
         success: function (response) {
             $('#report-content').val('');
             $('#success-report').foundation('reveal', 'open');
-            $('#report-material').foundation('reveal', 'close')
+            $('#report-material').foundation('reveal', 'close');
         },
         error: function (xhr, textStatus, thrownError) {
             alert('Error:' + xhr.statusText);
@@ -27,4 +27,59 @@ $(document).on('click', '#show-report-material', function () {
 
 $(document).on('click', '#close-sucess-report', function () {
     $('#success-report').foundation('reveal', 'close');
+});
+
+$(document).on('click', '.solve-report', function () {
+    var reportId = $(this).data('value');
+    var url = '/manage/reports/' + reportId + '/';
+    var csfrToken = $("input[name=csrfmiddlewaretoken]").val();
+    $.ajax({
+        url: url,
+        type: "PATCH",
+        headers: {
+            'X-CSRFToken': csfrToken
+        },
+        success: function () {
+            $('#report-card-' + reportId).hide();
+            var html = '<div data-alert class="alert-box success radius">';
+            html += 'El reporte ha sido marcado como resuelto de manera exitosa';
+            html += '<a href="#" class="close report-notif-close">&times;</a></div>';
+            $('#reports-notifications').append(html);
+        },
+        error: function (xhr, textStatus, thrownError) {
+            if (xhr.statusText) {
+                alert(textStatus);
+            }
+        }
+    });
+});
+
+$(document).on('click', '.reject-report', function () {
+    var reportId = $(this).data('value');
+    var url = '/manage/reports/' + reportId + '/';
+    var csfrToken = $("input[name=csrfmiddlewaretoken]").val();
+    $.ajax({
+        url: url,
+        type: "DELETE",
+        headers: {
+            'X-CSRFToken': csfrToken
+        },
+        success: function () {
+            $('#report-card-' + reportId).hide();
+            var html = '<div data-alert class="alert-box success radius">';
+            html += 'El reporte ha sido rechazado como error de manera exitosa';
+            html += '<a href="#" class="close report-notif-close">&times;</a></div>';
+            $('#reports-notifications').append(html);
+        },
+        error: function (xhr, textStatus, thrownError) {
+            if (xhr.statusText) {
+                alert(textStatus);
+            }
+        }
+    });
+});
+
+$(document).on('click', '.report-notif-close',function(){
+    var notification = this.parentNode;
+    notification.remove();
 });
